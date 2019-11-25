027e5ff32c9063e7b607db53d945f7200341a974
Juan Carlos Campos <rapidclimate@gmail.com>
2019-11-24 19:00:23 -0600

    Add files via upload

---
+++ precache-manifest.069e2261cb04f919cd51f1472445d8cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bdd5258d05f262bcb6",
+    "url": "/css/app.e2713bb0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c64b975e0bea438960d2c6bcf0e2b464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1bdd5258d05f262bcb6",
+    "url": "/js/app-legacy.fe31fc05.js"
+  },
+  {
+    "revision": "5cddc6459a680bf88ba9",
+    "url": "/js/chunk-vendors-legacy.dca9c05a.js"
+  },
+  {
+    "revision": "b5dc7ab270d79915cb7d8504a2814de8",
+    "url": "/manifest.json"
+  }
+]);
---
+++ precache-manifest.9f1ef255ff4239781308e83c2f852793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24263d9824873f7d7008",
+    "url": "/css/app.e2713bb0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7eab97db1e6d5d61b9753cc337433915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24263d9824873f7d7008",
+    "url": "/js/app.949253a2.js"
+  },
+  {
+    "revision": "e62d766ed379858c3fd5",
+    "url": "/js/chunk-vendors.c1688f9f.js"
+  },
+  {
+    "revision": "b5dc7ab270d79915cb7d8504a2814de8",
+    "url": "/manifest.json"
+  }
+]);
